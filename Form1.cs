ccff9c29225e17e665306f135b4e5292c0e743d8
NathanielFinuliar <nathaniel.finuliar14@gmail.com>
2022-07-19 20:45:37 +0800

    Using the package installed, I Added ResumeFile.json

--- Form1.cs
+++ Form1.cs
@@ -1,20 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
+﻿using Newtonsoft.Json;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
 
 namespace Pdf_Resume_Creator
 {
     public partial class Form1 : Form
     {
+        private readonly string _fileName = @"C:\Users\nathan\source\repos\Pdf-Resume-Creator\ResumeFile.json";
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public class Resume
+        {
+
         }
 
         private void CnvrtrLbl_Click(object sender, EventArgs e)
